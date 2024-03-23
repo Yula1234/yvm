ea50dda4b943331d5b48b415a24ae08331f048ba
Yula1234 <vpervuhina0@gmail.com>
2024-03-23 17:18:18 +0500

    Small updates

--- make_yvm.bat
+++ make_yvm.bat
@@ -4,5 +4,5 @@ echo Compiling yvm.nasm...
 nasm --gprefix _ -fwin32 ./yvm/yvm.nasm -o ./yvm/yvm.o
 if %ERRORLEVEL% == 0 (
 	echo Compiling main.c...
-	gcc ./yvm/main.c ./yvm/yvm.o -o ./yvm.exe -m32
+	gcc ./yvm/main.c ./yvm/yvm.o -o ./bin/yvm.exe -m32
 )
