0255b0a30a8082c2b5d698e845278d6c423a049d
Yula1234 <vpervuhina0@gmail.com>
2024-03-23 18:41:22 +0500

    New make script

--- make.bat
+++ make.bat
@@ -1,8 +1,4 @@
 @echo off
 
-echo Compiling yvm.nasm...
-nasm --gprefix _ -fwin32 ./yvm/yvm.nasm -o ./yvm/yvm.o
-if %ERRORLEVEL% == 0 (
-	echo Compiling main.c...
-	gcc ./yvm/main.c ./yvm/yvm.o -o ./bin/yvm.exe -m32
-)
+echo Compiling yvm...
+gcc ./yvm/main.c -o ./bin/yvm.exe -m32
